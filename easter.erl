6e160a99cd2428abc7ce20057e40f554628374a1
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-05-29 14:00:20 +0200

    Put divide and remainder into same function
    This algorithm used `A div B` and `A rem B` multiple times, to reduce the
    possibility of typos this is now put into the same function.

--- easter.erl
+++ easter.erl
@@ -9,16 +9,21 @@
 
 -spec when_is_easter(year()) -> date().
 
+-type non_zero_integer() :: pos_integer() | neg_integer().
+
+-spec divrem(Numerator :: integer(), Denominator :: non_zero_integer()) ->
+	{integer(), integer()}.
+
+divrem(Num, Den) ->
+	{Num div Den, Num rem Den}.
+
 when_is_easter(Year) ->
 	A = Year rem 19,
-	B = Year div 100,
-	C = Year rem 100,
-	D = B div 4,
-	E = B rem 4,
+	{B, C} = divrem(Year, 100),
+	{D, E} = divrem(B, 4),
 	G = (8 * B + 13) div 25,
 	H = (19 * A + B - D - G + 15) rem 30,
-	I = C div 4,
-	K = C rem 4,
+	{I, K} = divrem(C, 4),
 	L = (2 * E + 2 * I - H - K + 32) rem 7,
 	M = (A + 11 * H + 19 * L) div 433,
 	Month = (H + L - 7 * M + 90) div 25,
